c9a1433ec7ab72a3334123b5c9da7d11b8450a6d
Marc Casavant <marc@figmints.com>
2018-05-17 13:11:47 -0400

    refactor: Added props for bookShelf

--- src/components/bookShelf.js
+++ src/components/bookShelf.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Book from './book';
 
 function BookShelf (props) {
-
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.name}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    <Book />
+                </ol>
+            </div>
+        </div>
+    )
 }
 
 export default BookShelf;
